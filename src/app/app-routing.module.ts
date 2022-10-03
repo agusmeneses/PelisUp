82ebfaba7e089254beffb325768c84bea3a2f4cc
Agustin <agustinmeneses0@gmail.com>
2022-10-02 21:34:02 -0300

    Commit 1 del desafio 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { IngresarComponent } from './ingresar/ingresar.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { SeriesComponent } from './series/series.component';
+import { CardComponent } from './components/shared/card/card.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'inicio',component: InicioComponent,
+  },
+  {
+    path: 'series',component: SeriesComponent,
+  },
+  {
+    path: 'pelis',component: PeliculasComponent,
+  },
+  {
+    path: 'ingresar',component: IngresarComponent,
+  },
+  {
+    path: '',component: InicioComponent,
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './components/layout/layout.module';
+import { RoutesModule } from './components/routes/routes.module';
+import { SharedModule } from './components/shared/shared.module';
+import { InicioComponent } from './inicio/inicio.component';
+import { SeriesComponent } from './series/series.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SeriesComponent,
+    PeliculasComponent,
+    IngresarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    RoutesModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
